79b9733ac2c6e9dd3a8e9b3ac901534987a63e1a
robert <voicu.eduardrobert@gmail.com>
2020-12-03 10:43:12 +0200

    rest

---
+++ microservices/working/data/products.go
@@ -0,0 +1,54 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+// Product defines the structure for an API product
+type Product struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+// Products is a slice of products
+type Products []*Product
+
+// ToJSON writes products as JSON Format to w
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+//GetProducts return product list
+func GetProducts() Products {
+	return productList
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky caffee",
+		Price:       2.45,
+		SKU:         "abc123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.00,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
--- microservices/working/main.go
+++ microservices/working/main.go
@@ -1,30 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/voicurobert/GoProjects/microservices/working/handlers"
 )
 
-func main() {
-	// registers a function to a path on a DefaultServeMux (http handler)
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello world")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			//rw.WriteHeader(http.StatusBadRequest)
-			//rw.Write([]byte("Ooops"))
-			return
-		}
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+func oldHandlers() {
+	/*
+		// registers a function to a path on a DefaultServeMux (http handler)
+		http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+			log.Println("Hello world")
+			d, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(rw, "Ooops", http.StatusBadRequest)
+				//rw.WriteHeader(http.StatusBadRequest)
+				//rw.Write([]byte("Ooops"))
+				return
+			}
+			fmt.Fprintf(rw, "Hello %s", d)
+		})
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye world")
-	})
+		http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+			log.Println("Goodbye world")
+		})
+	*/
+}
+
+func main() {
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	//hh := handlers.NewHello(l)
+	//gh := handlers.NewGoodbye(l)
+	ph := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+	//sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// start the server
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("Received terminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	s.Shutdown(tc)
 
 	// web server, if handler is nill, the DefaultServeMux is used
-	http.ListenAndServe(":9090", nil)
+	//http.ListenAndServe(":9090", sm)
 }
